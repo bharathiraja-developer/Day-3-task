041a4c446a46c58b7a2c4d24759bc4660c5fff54
bharathiraja-developer <bharathiraja798@gmail.com>
2023-08-22 19:06:46 +0530

    Day3 tasks

--- task3.js
+++ task3.js
@@ -8,7 +8,7 @@ xhr.onload = function () {
     let data = JSON.parse(xhr.responseText);
 
     for (let detail of data) {
-       console.log('country name',detail.name);
+       console.log('country name : ',detail.name.common);
         console.log('region : ',detail.region);
         console.log('subregion : ',detail.subregion);
         console.log('population : ',detail.population);
