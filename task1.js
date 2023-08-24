4e6d9b48917fc9cd78aa3a68a8878c45b9e0da5d
bharathiraja-developer <bharathiraja798@gmail.com>
2023-08-24 11:57:00 +0530

    Day3 task

--- task1.js
+++ task1.js
@@ -10,23 +10,20 @@ Answer : In javaScript object notation we can able to store multiple values with
 let obj1 = { name: "Person1", age: 5 };
 let obj2 = { age: 5, name: "Person1" };
 
-let jsonObj1 = JSON.stringify(obj1);
-let jsonObj2 = JSON.stringify(obj2);
-
-if (typeof(jsonObj1) === typeof(jsonObj2)) {
-    console.log('type of both JSON is same');
-} else {
-    console.log('type of both JSON is not same');
+function compare(a, b) {
+    let result = true;
+    if (Object.keys(a).length == Object.keys(b).length) {
+        for (key in a) {
+            if (a[key] === b[key]) {
+                continue;
+            } else {
+                result = false;
+                break;
+            }
+        }
+    } else {
+        result = false;
+    }
+    return result;
 }
-
-if (obj1.name === obj2.name) {
-    console.log('both JSON name is same');
-} else {
-    console.log('both JSON name is not same');
-}
-
-if (obj1.age === obj2.age) {
-    console.log('both JSON age is same');
-} else {
-    console.log('both JSON age is not same');
-}
+console.log(compare(obj1, obj2));
